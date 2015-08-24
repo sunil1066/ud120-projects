69526dceb7f1eb7842f32b616168d1c2541b47d5
sunil1066 <jdyadav1993@gmail.com>
2015-08-24 21:42:43 +0530

    Update nb_author_id.py

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -22,6 +22,19 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+def submitAccuracy():
+    return acc
+
 
 
 
